7822a15babe3a76a1acf2c790c84af7a0e7055c4
kosnika25 <karenvaliaodeabreu@gmail.com>
2025-04-29 16:29:44 -0300

    commit

---
+++ src/firebase/connection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDusQ8hdZ5-N9Ukr2tI2TA-M3ZlMz0ED0E",
+  authDomain: "projetotarde-d15b4.firebaseapp.com",
+  projectId: "projetotarde-d15b4",
+  storageBucket: "projetotarde-d15b4.firebasestorage.app",
+  messagingSenderId: "943055935147",
+  appId: "1:943055935147:web:4139cf232deb427ec1c9ee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
